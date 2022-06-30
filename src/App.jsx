fea889e0183f9420afd4e2dbd7968e40277f48c2
Yuliia Boiko <littlehappinies@gmail.com>
2022-06-30 13:49:11 +0300

    Added script for Task 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import user from './data/user.json';
-import { Profile } from './components/Profile/index';
+import data from './data/data.json';
+import { Profile } from './components/Profile/index.jsx';
+import { Statistics } from './components/Statistics/index.jsx';
 
 export const App = () => {
   return (
@@ -12,6 +14,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
     </div>
   );
 };
